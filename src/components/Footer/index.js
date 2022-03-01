116d083976fc93c69dab6019ea4f038550e743ca
ciisiq <cii_3@hotmail.com>
2022-03-01 12:14:22 +0000

    size sidebar

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='flex justify-center items-center bg-slate-700 text-slate-50 w-screen h-16 md:h-20'>
+    <footer className="flex justify-center items-center bg-slate-700 text-slate-50 w-screen h-16 md:h-20">
       Footer
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -28,12 +28,19 @@ function Sidebar() {
       )}
 
       <div
-        className={`top-0 left-0 w-[35vw] bg-blue-600  p-10 pl-20 text-white fixed h-full z-40 ease-in-out duration-300 ${
+        className={`top-20 md:top-24  left-0  w-[55vw]  rounded-b-md bg-blue-600  p-10 pl-20 text-white fixed  z-40 ease-in-out duration-300 ${
           showSidebar ? "-translate-x-0 " : "-translate-x-full"
         }`}
       >
         <h3 className="mt-20 text-4xl font-semibold text-white">
-          I am a sidebar
+          <nav>
+            <ul>
+              <li className="py-4">Link 1</li>
+              <li className="py-4">Link 2</li>
+              <li className="py-4">Link 3</li>
+              <li className="py-4">Link 4</li>
+            </ul>
+          </nav>
         </h3>
       </div>
     </>
