1701a277964897d3ad7957920905b26f3633dd11
benmhcode <85193753+benmhcode@users.noreply.github.com>
2022-03-01 10:44:43 +0000

    initial setup

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import Sidebar from "../Sidebar";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Sidebar />
+      App
+    </div>
+  );
+}
+
+export default App;
